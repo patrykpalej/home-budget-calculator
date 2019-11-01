43474e018ef48a1ab075e8181b5a582cb4f38060
Patryk <patrykpalej@gmail.com>
2019-11-01 15:01:05 +0100

    part for preparing encoded dict separated

--- spendingsFinder.py
+++ spendingsFinder.py
@@ -12,13 +12,16 @@ myWorkbook = MyWorkbook(spendings_file_path)
 spend_values = myWorkbook.spends_values_yr
 spend_items = myWorkbook.spends_items_yr
 
+# 2. Preparation of list of encoded dictionaries of spendings
 
-# 2. Creating xlsx file for the results
+
+# 3. Creating xlsx file for the results
 output_xlsx = openpyxl.Workbook()
 ws = output_xlsx.active
 ws.title = "Przedział"
 
-# 3. Postprocessing
+
+# 4. Postprocessing
 results_dir = os.getcwd() + "/spendings_finder"
 if not os.path.exists(results_dir):
     os.mkdir(results_dir)
