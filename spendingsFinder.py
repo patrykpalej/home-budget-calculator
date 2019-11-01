c42abce670400ea772c3e6dc343211fc40ab1bcf
Patryk <patrykpalej@gmail.com>
2019-11-01 14:33:54 +0100

    Spendings values and labels extracted from myWorkbook object

--- spendingsFinder.py
+++ spendingsFinder.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import os
-import openpyxl
+from classes import *
 
 
-# 1. Importing config file
+# 1. Importing config file and total data
 config_txt = pd.read_csv("config/spends_to_find.txt", header=None)
 config_list = list(config_txt[0])
 
+spendings_file_path = os.getcwd() + "/data/total.xlsx"
+myWorkbook = MyWorkbook(spendings_file_path)
+spend_values = myWorkbook.spends_values_yr
+spend_items = myWorkbook.spends_items_yr
+
+
 # 2. Creating xlsx file for the results
 output_xlsx = openpyxl.Workbook()
+ws = output_xlsx.active
+ws.title = "Przedział"
 
 # 3. Postprocessing
 results_dir = os.getcwd() + "/spendings_finder"
@@ -16,4 +24,3 @@ if not os.path.exists(results_dir):
     os.mkdir(results_dir)
 
 output_xlsx.save(results_dir + "/Wydatki.xlsx")
-
