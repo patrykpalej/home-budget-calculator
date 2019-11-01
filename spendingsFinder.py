54bc15df3c7767466cca1ed25d0c0f9ab24c86c6
Patryk <patrykpalej@gmail.com>
2019-11-01 13:56:44 +0100

    saving results to xlsx file implemented

--- spendingsFinder.py
+++ spendingsFinder.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import os
+import openpyxl
 
 
+# 1. Importing config file
 config_txt = pd.read_csv("config/spends_to_find.txt", header=None)
 config_list = list(config_txt[0])
 
-print(config_list)
+# 2. Creating xlsx file for the results
+output_xlsx = openpyxl.Workbook()
+
+# 3. Postprocessing
+results_dir = os.getcwd() + "/spendings_finder"
+if not os.path.exists(results_dir):
+    os.mkdir(results_dir)
+
+output_xlsx.save(results_dir + "/Wydatki.xlsx")
+
