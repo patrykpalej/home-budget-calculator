0711a439c905e6877110a3005b9c39456a73d4f1
Patryk <patrykpalej@gmail.com>
2019-11-02 16:43:59 +0100

    tabs changed to spaces

--- spendingsFinder.py
+++ spendingsFinder.py
@@ -13,25 +13,33 @@ with open('spendings_finder/config.json') as handle:
 config_list = config_json["keywords"]
 period = config_json["period"]
 
-spendings_file_path = os.getcwd() + "/data/total.xlsx"
+spendings_file_path = os.getcwd() + "/data/total_test.xlsx"
 myWorkbook = MyWorkbook(spendings_file_path)
 spend_values = myWorkbook.spends_values_yr
 spend_items = myWorkbook.spends_items_yr
 
+
 # 2. Preparation of list of encoded dictionaries of spendings
 spends_dicts_list = list()
+# -----
+good_dict = {"cat_sums": [], "date_spends": []}
+
+
+
+
+# -----
 trial_dict = {"cat_sums": [["Mieszkanie i przyjemności", 250], ["Podróże",
-														20], ["Ubrania", 150]],
-			  "date_spends": {"Styczeń 2053": [["szermierka", 12],
-											   ["spodnie", 120],
-											   ["audiobook", 40]],
-							  "Marzec 2053": [["herbaciarnia", 12],
-											  ["teatr", 34]],
-							  "Lipiec 2053": [["Wyjście na piwo", 20]],
-							  "Luty 20154": [["katowice, przejazd", 1],
-											 ["wydatki", 14]]
-							  }
-			  }
+                                                        20], ["Ubrania", 150]],
+              "date_spends": {"Styczeń 2053": [["szermierka", 12],
+                                               ["spodnie", 120],
+                                               ["audiobook", 40]],
+                              "Marzec 2053": [["herbaciarnia", 12],
+                                              ["teatr", 34]],
+                              "Lipiec 2053": [["Wyjście na piwo", 20]],
+                              "Luty 20154": [["katowice, przejazd", 1],
+                                             ["wydatki", 14]]
+                              }
+              }
 spends_dicts_list.append(trial_dict)
 
 # 3. Creating xlsx file for the results
@@ -46,62 +54,62 @@ thin_border = Border(left=Side(style='thin'),
 
 # a) cat_sums table
 try:
-	ws.column_dimensions["A"].width = max([len(cat[0]) for cat in
-										   trial_dict["cat_sums"]]) + 2
+    ws.column_dimensions["A"].width = 2 + max([len(cat[0])
+                           for cat in trial_dict["cat_sums"]])
 except:
-	pass
+    pass
 
 row = 1
 for cat in trial_dict["cat_sums"]:
-	ws.cell(row, 1).value = cat[0]
-	ws.cell(row, 2).value = cat[1]
+    ws.cell(row, 1).value = cat[0]
+    ws.cell(row, 2).value = cat[1]
 
-	ws.cell(row, 1).border = thin_border
-	ws.cell(row, 2).border = thin_border
+    ws.cell(row, 1).border = thin_border
+    ws.cell(row, 2).border = thin_border
 
-	ws.cell(row, 1).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
+    ws.cell(row, 1).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
 
-	row += 1
+    row += 1
 
 # b) date_spends table
 row = 1
 for date in list(trial_dict["date_spends"].keys()):
-	ws.merge_cells(start_row=row, start_column=8, end_row=row, end_column=9)
-	ws.cell(row, 8).value = date
-	ws.cell(row, 8).alignment = Alignment(horizontal='center')
-	ws.cell(row, 8).fill = PatternFill(fgColor='93e1e6', fill_type='solid')
-	ws.cell(row, 8).border = thin_border
-	row += 1
+    ws.merge_cells(start_row=row, start_column=8, end_row=row, end_column=9)
+    ws.cell(row, 8).value = date
+    ws.cell(row, 8).alignment = Alignment(horizontal='center')
+    ws.cell(row, 8).fill = PatternFill(fgColor='93e1e6', fill_type='solid')
+    ws.cell(row, 8).border = thin_border
+    row += 1
 
-	ws.cell(row, 8).value = "Co?"
-	ws.cell(row, 9).value = "Kwota [zł]"
-	ws.cell(row, 8).alignment = Alignment(horizontal='center')
-	ws.cell(row, 9).alignment = Alignment(horizontal='center')
-	ws.cell(row, 8).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
-	ws.cell(row, 9).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
-	ws.cell(row, 8).border = thin_border
-	ws.cell(row, 9).border = thin_border
+    ws.cell(row, 8).value = "Co?"
+    ws.cell(row, 9).value = "Kwota [zł]"
+    ws.cell(row, 8).alignment = Alignment(horizontal='center')
+    ws.cell(row, 9).alignment = Alignment(horizontal='center')
+    ws.cell(row, 8).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
+    ws.cell(row, 9).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
+    ws.cell(row, 8).border = thin_border
+    ws.cell(row, 9).border = thin_border
 
-	row += 1
-	max_length = 0
-	for spend in trial_dict["date_spends"][date]:
-		ws.cell(row, 8).value = spend[0]
-		ws.cell(row, 9).value = spend[1]
-		ws.cell(row, 8).border = thin_border
-		ws.cell(row, 9).border = thin_border
+    row += 1
+    max_length = 0
+    for spend in trial_dict["date_spends"][date]:
+        ws.cell(row, 8).value = spend[0]
+        ws.cell(row, 9).value = spend[1]
+        ws.cell(row, 8).border = thin_border
+        ws.cell(row, 9).border = thin_border
 
-		max_length = max(max_length, len(spend[0]))
-		row += 1
+        max_length = max(max_length, len(spend[0]))
+        row += 1
 
 try:
-	ws.column_dimensions["H"].width = max_length + 2
+    ws.column_dimensions["H"].width = max_length + 2
 except:
-	pass
+    pass
 
 
 # 4. Postprocessing
 results_dir = os.getcwd() + "/spendings_finder"
 if not os.path.exists(results_dir):
-	os.mkdir(results_dir)
+    os.mkdir(results_dir)
 
 output_xlsx.save(results_dir + "/Przedział czasu.xlsx")
