9dd4acbf9d03ae5933b6871477314ed03be323af
Patryk <patrykpalej@gmail.com>
2019-11-02 21:27:15 +0100

    exporting xlsx file basing on real input data implemented

--- spendingsFinder.py
+++ spendingsFinder.py
@@ -22,104 +22,99 @@ spend_monthlabels = myWorkbook.spends_monthlabel_yr
 
 # 2. Preparation of list of encoded dictionaries of spendings
 spends_dicts_list = list()
-phrase = "test"
-# -----
-good_dict = {"cat_sums": {}, "date_spends": {}}
 
-for cat in spend_items.keys():
-    for i, item in enumerate(spend_items[cat]):
-        if phrase.lower() in item.lower():
-            if cat in good_dict["cat_sums"].keys():
-                good_dict["cat_sums"][cat] += spend_values[cat][i]
-            else:
-                good_dict["cat_sums"][cat] = spend_values[cat][i]
+for phrase in config_list:
+    phrase_dict = {"cat_sums": {}, "date_spends": {}}
 
-            if spend_monthlabels[cat][i] in good_dict["date_spends"].keys():
-                good_dict["date_spends"][spend_monthlabels[cat][i]]\
-                    .append([spend_items[cat][i], spend_values[cat][i]])
-            else:
-                good_dict["date_spends"][spend_monthlabels[cat][i]] \
-                    = [[spend_items[cat][i], spend_values[cat][i]]]
+    for cat in spend_items.keys():
+        for i, item in enumerate(spend_items[cat]):
+            if phrase.lower() in item.lower():
+                if cat in phrase_dict["cat_sums"].keys():
+                    phrase_dict["cat_sums"][cat] += spend_values[cat][i]
+                else:
+                    phrase_dict["cat_sums"][cat] = spend_values[cat][i]
 
-# -----
-trial_dict = {"cat_sums": {"Mieszkanie i przyjemności": 250, "Podróże": 20,
-                           "Ubrania": 150},
-              "date_spends": {"Styczeń 2053": [["szermierka", 12],
-                                               ["spodnie", 120],
-                                               ["audiobook", 40]],
-                              "Marzec 2053": [["herbaciarnia", 12],
-                                              ["teatr", 34]],
-                              "Lipiec 2053": [["Wyjście na piwo", 20]],
-                              "Luty 20154": [["katowice, przejazd", 1],
-                                             ["wydatki", 14]]
-                              }
-              }
-spends_dicts_list.append(trial_dict)
+                if spend_monthlabels[cat][i] \
+                        in phrase_dict["date_spends"].keys():
+                    phrase_dict["date_spends"][spend_monthlabels[cat][i]]\
+                        .append([spend_items[cat][i], spend_values[cat][i]])
+                else:
+                    phrase_dict["date_spends"][spend_monthlabels[cat][i]] \
+                        = [[spend_items[cat][i], spend_values[cat][i]]]
+
+    spends_dicts_list.append(phrase_dict)
 
 # 3. Creating xlsx file for the results
 output_xlsx = openpyxl.Workbook()
-ws = output_xlsx.active
-ws.title = "Słowo"
-
 thin_border = Border(left=Side(style='thin'),
                      right=Side(style='thin'),
                      top=Side(style='thin'),
                      bottom=Side(style='thin'))
 
-# a) cat_sums table
-try:
-    ws.column_dimensions["A"].width = 2 + max([len(cat) for cat in
-                                               trial_dict["cat_sums"].keys()])
-except:
-    pass
+for p, phrase in enumerate(config_list):
+    max_col_width = 0
 
-row = 1
-for cat in trial_dict["cat_sums"].keys():
-    ws.cell(row, 1).value = cat
-    ws.cell(row, 2).value = trial_dict["cat_sums"][cat]
+    ws = output_xlsx[output_xlsx.sheetnames[-1]]
+    ws.title = phrase
+    phrase_dict = spends_dicts_list[p]
 
-    ws.cell(row, 1).border = thin_border
-    ws.cell(row, 2).border = thin_border
+    # a) cat_sums table
+    try:
+        ws.column_dimensions["A"].width = max([len(cat) for cat in
+                                               phrase_dict["cat_sums"].keys()])
+    except ValueError:
+        pass
 
-    ws.cell(row, 1).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
+    row = 1
+    for cat in phrase_dict["cat_sums"].keys():
+        ws.cell(row, 1).value = cat
+        ws.cell(row, 2).value = phrase_dict["cat_sums"][cat]
 
-    row += 1
+        ws.cell(row, 1).border = thin_border
+        ws.cell(row, 2).border = thin_border
 
-# b) date_spends table
-row = 1
-for date in list(trial_dict["date_spends"].keys()):
-    ws.merge_cells(start_row=row, start_column=8, end_row=row, end_column=9)
-    ws.cell(row, 8).value = date
-    ws.cell(row, 8).alignment = Alignment(horizontal='center')
-    ws.cell(row, 8).fill = PatternFill(fgColor='93e1e6', fill_type='solid')
-    ws.cell(row, 8).border = thin_border
-    row += 1
+        ws.cell(row, 1).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
 
-    ws.cell(row, 8).value = "Co?"
-    ws.cell(row, 9).value = "Kwota [zł]"
-    ws.cell(row, 8).alignment = Alignment(horizontal='center')
-    ws.cell(row, 9).alignment = Alignment(horizontal='center')
-    ws.cell(row, 8).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
-    ws.cell(row, 9).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
-    ws.cell(row, 8).border = thin_border
-    ws.cell(row, 9).border = thin_border
+        row += 1
 
-    row += 1
-    max_length = 0
-    for spend in trial_dict["date_spends"][date]:
-        ws.cell(row, 8).value = spend[0]
-        ws.cell(row, 9).value = spend[1]
+    # b) date_spends table
+    row = 1
+    for date in sorted(phrase_dict["date_spends"].keys()):
+        # !!! convert number of month into {Month yyyy} format
+
+        ws.merge_cells(None, row, 8, row, 9)
+        ws.cell(row, 8).value = date
+        ws.cell(row, 8).alignment = Alignment(horizontal='center')
+        ws.cell(row, 8).fill = PatternFill(fgColor='93e1e6', fill_type='solid')
+        ws.cell(row, 8).border = thin_border
+        row += 1
+
+        ws.cell(row, 8).value = "Co?"
+        ws.cell(row, 9).value = "Kwota [zł]"
+        ws.cell(row, 8).alignment = Alignment(horizontal='center')
+        ws.cell(row, 9).alignment = Alignment(horizontal='center')
+        ws.cell(row, 8).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
+        ws.cell(row, 9).fill = PatternFill(fgColor='daf6f7', fill_type='solid')
         ws.cell(row, 8).border = thin_border
         ws.cell(row, 9).border = thin_border
 
-        max_length = max(max_length, len(spend[0]))
         row += 1
+        for spend in phrase_dict["date_spends"][date]:
+            ws.cell(row, 8).value = spend[0]
+            ws.cell(row, 9).value = spend[1]
+            ws.cell(row, 8).border = thin_border
+            ws.cell(row, 9).border = thin_border
 
-try:
-    ws.column_dimensions["H"].width = max_length + 2
-except:
-    pass
+            max_col_width = max(max_col_width, len(spend[0]))
+            row += 1
 
+    try:
+        ws.column_dimensions["H"].width = max_col_width
+    except NameError:
+        pass
+
+    if len(config_list)-2 >= p:
+        output_xlsx.create_sheet("name", p+1)
 
 # 4. Postprocessing
 results_dir = os.getcwd() + "/spendings_finder"
