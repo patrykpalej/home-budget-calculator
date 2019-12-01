2bb3a5c19d26626f008c0ee401f9e930a381fcc0
Patryk Palej <patrykpalej@gmail.com>
2019-12-01 10:51:47 +0100

    Change name of folder with results in month scenario

--- monthAnalysis.py
+++ monthAnalysis.py
@@ -133,7 +133,7 @@ subcats_labels_2 = [list(subcats_dict.keys())[i]+' - '
                     for i in range(len(list(subcats_dict.keys())))]
 
 # 3. Visualization and saving the plots
-results_dir = folder_path + "/!Raporty/month_results/" + month_label \
+results_dir = folder_path + "/!Raporty/monthly_reports/" + month_label \
               + " - wyniki"
 if not os.path.exists(results_dir):
     os.mkdir(results_dir)
