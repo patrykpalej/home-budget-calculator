7fa4e2152d7e537d6330e2a034343d01868bf2ef
Patryk Palej <patrykpalej@gmail.com>
2020-05-16 14:17:00 +0200

    Implement running month analysis without any parameters

--- monthAnalysis.py
+++ monthAnalysis.py
@@ -8,8 +8,18 @@ from functions.month_funcs.create_all_plots import create_all_plots
 from functions.month_funcs.create_pptx_presentation \
     import create_pptx_presentation
 
-month_num = sys.argv[1]
-year_num = sys.argv[2]
+try:
+    month_num = sys.argv[1]
+    year_num = sys.argv[2]
+except IndexError:
+    folder_path_file = open("path.txt", "r")
+    folder_path = folder_path_file.read()
+    folder_path_file.close()
+    folder_path += "/monthly data/"
+    second_newest = os.listdir(folder_path)[-2]
+
+    year_num = second_newest[:2]
+    month_num = second_newest[5:7]
 
 
 # Preparing the analysis
